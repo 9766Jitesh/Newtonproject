801a31b8bb5ef037c31b66781cd3371b8de5cd53
9766jitesh <9766jitesh@gmail.com>
2022-07-12 23:45:52 +0530

    first

---
+++ first_1.java
@@ -0,0 +1,33 @@
+import java.util.*;
+//import java.i/o.*;
+public class first_1{
+public static void main(String [] args){
+			Scanner sc=new Scanner(System.in);
+			int n=sc.nextInt();
+int a=0;
+			while(n>0){
+			 a=n%10;
+			n=n/10;
+			//System.out.println(a);
+
+         			
+        
+    
+switch(a){
+case 1:
+System.out.print("one");
+break;
+case 2:
+System.out.print("two");
+break;
+case 3:
+System.out.print("three");
+break;
+case 4:
+System.out.print("four");
+break;
+
+}
+}
+}
+}
